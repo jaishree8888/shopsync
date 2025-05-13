8a6d62ad9be9091b38eafcd20ee5f97d45c5c581
jaishree8888 <jaishreeyuva88@gmail.com>
2025-05-13 19:37:46 +0530

    cookies removed

--- shopsync-backend/server.js
+++ shopsync-backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000', 
   credentials: true, // Allow cookies
 }));
 app.use(express.json());
