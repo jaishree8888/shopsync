f623614bfd5fca27e26cdf513d8a630bd4a24e42
jaishree8888 <jaishreeyuva88@gmail.com>
2025-07-24 23:10:38 +0530

    "Final Commit"

--- shopsync-backend/server.js
+++ shopsync-backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://shopsync-fr.vercel.app/login',
   credentials: true,
 }));
 app.use(express.json());
