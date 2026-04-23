6779a8cd8131c5e645a67075efdd9dbe4980bb91
jaishree8888 <jaishreeyuva88@gmail.com>
2026-04-23 18:14:46 +0530

    fixed cors issue completely

--- shopsync-backend/server.js
+++ shopsync-backend/server.js
@@ -7,8 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://shopsync-jade.vercel.app/login', 'http://localhost:5173'],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
